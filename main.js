dd2e27a0ace1d05fde4eae0b2059acecac3136a3
LeannePepper <leanne.werner.lw@gmail.com>
2019-03-04 13:52:50 -0800

    added styles to directory preview

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
  const employeeCards = document.querySelector('.employee-cards'); 
 
+ function capitalize(item) {
+    let newWord = item[0].toUpperCase() + item.slice(1);
+    return newWord; 
+  }
+
 
 // Getting data from the random users API 
 fetch('https://randomuser.me/api/?seed=foobar&results=12&inc=picture,name,email,location,phone,dob,')
@@ -9,9 +14,10 @@ fetch('https://randomuser.me/api/?seed=foobar&results=12&inc=picture,name,email,
         data.results.forEach(function(employee){
             console.log(employee);
             let photo = employee.picture.large;
-            let firstName = employee.name.first;
-            let lastName = employee.name.last;
-            let city = employee.location.city;
+            let firstName = capitalize(employee.name.first);
+            let lastName = capitalize(employee.name.last);
+            let fullName = firstName + " " + lastName;
+            let city = capitalize(employee.location.city);
             let email = employee.email;
             let phone = employee.phone;
             let address = employee.location.street + "," + " " + employee.location.state + " " + employee.location.postcode;
@@ -23,11 +29,10 @@ fetch('https://randomuser.me/api/?seed=foobar&results=12&inc=picture,name,email,
             const html = `
             <img class="thumbnail" src='${photo}' alt='employee photo'>
             <div class="employee-preview-information">
-                <span class="first-name"> ${firstName}</span> 
-                <span class="last-name"> ${lastName} </span>
+                <span class="full-name"> <strong>${fullName}</strong></span>  
                 <div class="email"> ${email} </div>
                 <div class="city"> ${city} </div>
-            <div>
+            </div>
         `;
         card.innerHTML = html;
         employeeCards.appendChild(card); 
